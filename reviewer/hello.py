08136cbdf579a2c70349a20abe9fe7a780240729
Arun Arya (EXT-Nokia) <arun.arya.ext@nokia.com>
2025-10-08 22:29:20 +0530

    feat: PR reviewer testing

--- reviewer/hello.py
+++ reviewer/hello.py
@@ -2,7 +2,7 @@ def say_hello():
     """
     Prints a greeting message.
     """
-    print("Hello, Python!")
+    print("Hello, Python!, test PR review system is working.")
 
 # Run the function when the script is executed directly
 if __name__ == "__main__":

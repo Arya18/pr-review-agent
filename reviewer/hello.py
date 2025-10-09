23d450e5bd1fe9cad7988796cf65dbe66ca0eafe
Arun Arya (EXT-Nokia) <arun.arya.ext@nokia.com>
2025-10-09 16:54:35 +0530

    feat: test inline review

--- reviewer/hello.py
+++ reviewer/hello.py
@@ -4,5 +4,13 @@ def say_hello():
     """
     print("Hello, Python!, sda sdvaae ew `.")
 
+# app.py
+
+def add(a, b):
+    c = a + b
+    return c  
+
+
 if __name__ == "__main__":
     say_hello()
+    add(3, 4)
